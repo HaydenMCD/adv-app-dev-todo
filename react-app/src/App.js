a7dec78b2b3a10b643dc210d05a92daa6b3ada5f
Hayden-McD <mcdohr2@student.op.ac.nz>
2022-03-04 17:51:06 +1300

    Added css styling (Will be changed later), created component for input field and uploading to firebase.

--- react-app/src/App.js
+++ react-app/src/App.js
@@ -1,12 +1,16 @@
 import './App.css';
 import React from "react";
 import Title from "./Components/title";
+import AddTodo from './Components/AddTodo';
 
 function App() {
   return (
     <div className="App">
       <div>
         <Title />
+      </div>
+      <div>
+        <AddTodo />
       </div>
     </div>
   );
